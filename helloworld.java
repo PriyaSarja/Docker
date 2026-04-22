0d0930a5d25c60e872f5af22fb78febeca9af128
Priyadarshani Sarja <146539142+PriyaSarja@users.noreply.github.com>
2026-04-22 16:00:05 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+public class Fibonacci {                
+    public static void main(String[] args) {                
+        int n = 10, t1 = 0, t2 = 1;                
+        System.out.print("Hello World!");                
+                      
+    }                
+}
